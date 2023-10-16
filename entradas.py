dad84f885f0fb510175ca3d9be1758a344926235
Darwin12cr <133026929+Darwin12cr@users.noreply.github.com>
2023-10-16 18:05:43 -0400

    Update entradas.py

--- entradas.py
+++ entradas.py
@@ -1,3 +1,15 @@
 print("Bienvenido al Programa de BienvenidA")
 name = input("Ingrese su nombre: ")
 print("bienvenido Señor " + name)
+
+
+print("Programa de Calculadora")
+primer_num = input("Ingre 1er Numero: ")
+segundo_num = input("Ingrese 2do Numero: ")
+print(int(primer_num) + int(segundo_num))
+
+parsecs_input = input('Ingrese el Valor de parsecs')
+parsecs= parsecs_input
+aniosLuz = parsecs * 3.26
+
+print(str(parsecs) + " parsecs son " + str(aniosLuz) + " años luz")
