142f58f0f66dc20ea10880377cfe019a2e0a65b6
Darwin12cr <133026929+Darwin12cr@users.noreply.github.com>
2023-10-17 17:47:45 -0400

    Create program.py

---
+++ scr/program.py
@@ -0,0 +1,12 @@
+#1er programa py
+from datetime import date
+
+sum=1+2 #3
+product= sum*2
+print (product)
+
+left_side = 10
+right_side = 5
+print (left_side + right_side) # 2
+
+print('La fecha de hoy es : '+ str(date.today()))
